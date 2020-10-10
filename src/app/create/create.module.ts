61ec310814b8d548d5606715d0164dd443d8ee68
TatsuyaSSK <ssktty514@gmail.com>
2020-10-10 22:30:00 +0900

    commit

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create/create.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 @NgModule({
@@ -13,6 +16,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CreateRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule,
+
   ]
 })
 export class CreateModule { }
