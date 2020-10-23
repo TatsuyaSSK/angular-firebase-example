3f8aa5123b90dec2d1008b67b3afc00efd995f37
TatsuyaSSK <ssktty514@gmail.com>
2020-10-23 20:06:04 +0900

    feat(firebase): firebaseとangularの連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCG0JmJweQQWuhr94Mf4Ae8vh0U_IHx9Cc",
+    authDomain: "gitpet-d4234.firebaseapp.com",
+    databaseURL: "https://gitpet-d4234.firebaseio.com",
+    projectId: "gitpet-d4234",
+    storageBucket: "gitpet-d4234.appspot.com",
+    messagingSenderId: "397927722048",
+    appId: "1:397927722048:web:1a5cbb7b7db99773b2c3a5",
+    measurementId: "G-Q2MJMSX0QY"
+  },
 };
 
 /*
