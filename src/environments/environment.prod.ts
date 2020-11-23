84b6b3f96aedfdbe5650623ee0630cd72e69dfb6
TatsuyaSSK <ssktty514@gmail.com>
2020-11-23 15:35:37 +0900

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCG0JmJweQQWuhr94Mf4Ae8vh0U_IHx9Cc",
+    authDomain: "gitpet-d4234.firebaseapp.com",
+    databaseURL: "https://gitpet-d4234.firebaseio.com",
+    projectId: "gitpet-d4234",
+    storageBucket: "gitpet-d4234.appspot.com",
+    messagingSenderId: "397927722048",
+    appId: "1:397927722048:web:1a5cbb7b7db99773b2c3a5",
+    measurementId: "G-Q2MJMSX0QY"
+  },
 };
